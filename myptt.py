4cd7799ef32265302c9d9658a04f809a6786b836
Your Name <you@example.com>
2020-11-22 15:52:12 +0800

    可蒐集各定版和ALLPOST

---
+++ myptt.py
@@ -0,0 +1,195 @@
+'''
+0. settings.py
+    - request
+        - 逾期嘗試
+        - 錯誤嘗試
+        - 間隔
+    - 目標網站domain
+    - async數量
+    - log
+        - 儲存位置
+        - 格式
+        - 層級
+1. Read list
+    - 從檔案
+    - 從server
+2. 異步搜集
+    - paramater
+        - 一次可同時多少個
+        - 蒐集格式
+        - 權限
+    - 錯誤處理
+3. 處理蒐集資料
+    - 欄位過濾
+    - 轉換成指定格式
+    - 儲存至
+4. 拿取下個蒐集目標
+'''
+import os
+import configparser
+config = configparser.ConfigParser()
+config.read(os.environ['SETTING'])
+config['LOG']['path'] = os.environ.get('LOG_PATH', None) or os.path.dirname(os.path.abspath(__file__))+'/logs'
+os.environ['LOG_PATH'] = config['LOG']['path']
+with open(os.environ['SETTING'], 'w') as f:
+    config.write(f)
+
+import logging
+import logging.config
+
+logging.config.fileConfig(
+    os.path.dirname(os.path.abspath(__file__))+'/'+config['LOG']['config'])
+logger = logging.getLogger(__name__)
+
+import sys
+import traceback
+import argparse
+import httpx
+import asyncio
+import time
+import re
+import json
+from bs4 import BeautifulSoup
+
+
+from tool import web_parse
+
+
+COOKIES = {'over18': '1'}
+
+
+async def main(url, *, from_page=0, to_page=1, all_post=False, board_name=None):
+    posts_info = [] # 蒐集到的文章資訊
+    if all_post:
+        per_page = int(config['PTT_ALLPOST']['per_page']) # 一次蒐集幾頁的文章連結，一頁最多20篇文章連結
+        base_url = config['PTT_ALLPOST']['url']
+    elif board_name:
+        per_page = int(config['PTT_BOARD']['per_page']) # 一次蒐集幾頁的文章連結，一頁最多20篇文章連結
+        base_url = config['PTT_BOARD']['url'] + '/' + board_name
+    if not from_page:
+    # 拿取index.html的初始頁面和前一頁頁數
+        with httpx.Client(cookies=COOKIES, timeout=int(config['REQUEST']['timeout'])) as client:
+            oldest, prev, next_, latest = fetch_last_page(client, url)
+            start = int(latest)
+    else:
+        start = int(from_page)
+
+    end = int(to_page)
+    # 產生要蒐集的頁數連結
+    cur_page = start
+    while cur_page >= end:
+        post_links = []  # [link]
+        links = []
+        for page_num in range(cur_page, max(cur_page-per_page, end-1), -1):
+            links.append((page_num, base_url+f'/index{page_num}.html'))
+
+        logger.debug(links)
+        async with httpx.AsyncClient(cookies=COOKIES, timeout=int(config['REQUEST']['timeout'])) as client:
+            # 蒐集每一頁的文章連結
+            tasks = [asyncio.create_task(fetch_post_list(client, page_num, link)) for page_num, link in links]
+            try:
+                result = await asyncio.gather(*tasks, return_exceptions=True)
+            except asyncio.TimeoutError:
+                logger.error(traceback.format_exception(*sys.exc_info()))
+            except Exception:
+                logger.error(traceback.format_exception(*sys.exc_info()))
+            else:
+                web_parse.parse_post_links(result, post_links=post_links, all_post=all_post)
+                logger.debug(f'共有 {len(post_links)} 篇文章要蒐集')
+
+            # 蒐集現有文章連結內容
+            tasks = [asyncio.create_task(fetch_post_content(
+                client, link)) for link in post_links]
+            try:
+                result = await asyncio.gather(*tasks)
+            except asyncio.TimeoutError:
+                logger.error(traceback.format_exception(*sys.exc_info()))
+            except Exception:
+                logger.error(traceback.format_exception(*sys.exc_info()))
+            else:
+                web_parse.parse_posts(result, posts_info=posts_info)
+                await asyncio.to_thread(record, 'result.rec', posts_info)
+
+            cur_page -= per_page
+
+def fetch_last_page(client, /, url):
+    try:
+        response = client.get(url)
+        response.raise_for_status()
+    except httpx.RequestError as exc:
+        etype, value, tb = sys.exc_info()
+        logger.error(f'蒐集最新 {exc.request.url} 文章列表失敗: {etype}')
+    except httpx.HTTPStatusError as exc:
+        logger.error(f'蒐集最新 {exc.request.url} 文章列表失敗: {exc.response.status_code}')
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        page_nums = web_parse.parse_page_num(soup)
+
+    return page_nums
+
+
+async def fetch_post_list(client, /, page_num, url):
+    try:
+        response = await client.get(url)
+        response.raise_for_status()
+    except httpx.RequestError as exc:
+        etype, value, tb = sys.exc_info()
+        logger.error(f'蒐集 {exc.request.url} 文章列表失敗: {etype}')
+    except httpx.HTTPStatusError as exc:
+        logger.error(f'蒐集 {exc.request.url} 文章列表失敗: {exc.response.status_code}')
+    else:
+        return response
+
+async def fetch_post_content(client, /, url):
+    try:
+        response = await client.get(url)
+        response.raise_for_status()
+    except httpx.RequestError as exc:
+        etype, value, tb = sys.exc_info()
+        logger.error(f'蒐集 {exc.request.url} 文章失敗: {etype}')
+    except httpx.HTTPStatusError as exc:
+        logger.error(f'蒐集 {exc.request.url} 文章失敗: {exc.response.status_code}')
+    else:
+        return response
+
+def record(file, r):
+    with open(file, 'a') as f:
+        json.dump(r, f, ensure_ascii=False)
+
+
+def read_list(file):
+    with open(file, 'r') as f:
+        pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--list', help="蒐集清單檔案", default=None)
+    parser.add_argument('-a', '--allpost', help="從allpost板中蒐集文章", action='store_true') # TODO: 測試版先測allpost，所以不指定參數時預設為開啟，正式版要改回store_true
+    parser.add_argument('--hotboard', help="蒐集熱門清單", action='store_true')
+    parser.add_argument('-b', '--board', help="蒐集指定板，板名不分大小寫", default=None)
+    parser.add_argument('-u', '--url', help="要爬取的網頁url", default=None)
+    args = parser.parse_args()
+
+    main_url = None
+    if args.list:
+        read_list(args.list)
+        sys.exit(0)
+    elif args.allpost:
+        main_url = config['PTT_ALLPOST']['url']+'/index.html'
+    elif args.board:
+        main_url = config['PTT_BOARD']['url']+args.board+'/index.html'
+    elif args.hotboard:
+        main_url = config['PTT_HOTBOARD']['url']+'/hotboards.html'
+        sys.exit(0)
+    elif args.url:
+        main_url = args.url
+    else:
+       parser.print_help()
+       sys.exit(0)
+
+    start = time.time()
+    asyncio.run(main(main_url, from_page=39505, to_page=39500, all_post=args.allpost, board_name=args.board))
+    end = time.time()
+    print(f'start: {start}, end: {end}, total: {end-start}')
+
+
---
+++ tool/web_parse/__init__.py
@@ -0,0 +1,130 @@
+import logging
+import os
+import re
+import bs4
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
+import configparser
+config = configparser.ConfigParser()
+config.read(os.environ['SETTING'])
+logger = logging.getLogger(__name__)
+
+
+def parse_page_num(soup):
+    # 0: 最舊, 1: 上頁, 2: 下頁, 3: 最新
+    pages = []
+    for paging_bar in soup.find_all('div', class_='btn-group-paging'):
+        paging = paging_bar.find_all('a')
+        hrefs = [href.get('href') if href.get('href')
+                 else None for href in paging]
+        for href in hrefs:
+            if not href:
+                pages.append(None)
+                continue
+            page_pattern = re.search(r'index([\d]+).html', href)
+            if not page_pattern:
+                pages.append(None)
+                continue
+            pages.append(page_pattern.groups()[0])
+    if not pages[3]:
+        pages[3] = str(int(pages[1])+1)
+    return pages
+
+def parse_post_links(responses, *, post_links: list[str], all_post=False):
+
+    for response in responses:
+        if not response:
+            continue
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for title_div in soup.find_all('div', class_='title'):
+            if not title_div.find('a'):
+                continue
+            title = title_div.find('a').get_text(strip=True)
+            post_link = title_div.find('a').get('href')
+
+            if all_post:
+                board_block = re.search(r'\(([\w\-]+)\)$', title)
+                if board_block:
+                    board = board_block.groups()[0]
+                    post_link = config['PTT']['url'] + post_link.replace('ALLPOST', board)
+                    post_links.append(post_link)
+            else:
+                post_link = config['PTT']['url'] + post_link
+                post_links.append(post_link)
+
+    #logger.debug(responses)
+    #logger.debug(post_links)
+
+def parse_posts(responses, *, posts_info: list):
+    for response in responses:
+        post_info = defaultdict(list)
+        if not response:
+            continue
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        #logger.debug(response.url)
+        # 文章上方資訊
+        for meta in soup.find_all('div', class_='article-metaline'):
+            author_meta = re.search(r'作者\s*([^\s]+)', meta.get_text(strip=True))
+            title_meta = re.search(r'標題\s*((?:Re|RE|re)\:)?\s*(\[[\w\-]+\])?\s*(.*)', meta.get_text(strip=True))
+            time_meta = re.search(r'時間\s*([a-zA-Z\d: ]+)$', meta.get_text(strip=True))
+            if author_meta:
+                post_info['author'] = author_meta.groups()[0]
+            elif title_meta:
+                post_info['is_reply'] = title_meta.groups()[0] != None
+                post_info['category'] = title_meta.groups()[1]
+                post_info['title'] = title_meta.groups()[2].strip()
+            elif time_meta:
+                post_info['time'] = time_meta.groups()[0].strip()
+        # 原文和回覆區的分界線
+        content_ip_bottom = None
+        # f2為class都為「※」相關資訊，例如:發信站、文章網址、引述、編輯
+        f2_meta = soup.select('div#main-content > span.f2') or []
+        # 找原po ip訊息
+        for f2 in f2_meta:
+            ip_meta = re.search(r'([\d]+\.[\d]+\.[\d]+\.[\d]+)', f2.get_text(strip=True))
+            if ip_meta:
+                post_info['ip'].append(ip_meta.groups()[0])
+                # 若發信站後接的是文章網址訊息，則視為該f2 element是原文和回覆區的分界線
+                if f2.next_sibling and f2.find_next_siblings('a') and f2.find_next_siblings('a')[0].get('href') == response.url:
+                    content_ip_bottom = f2
+
+        # 原文和回覆區的分界線被原po砍掉了，則採用最後編輯以上為原文
+        if not content_ip_bottom:
+            content_ip_bottom = f2
+        # 拿取回覆區塊以上和以下的原文訊息，不會有引述和其它轉發的回覆在內，也不會有a tag裡的連結
+        content = []
+        post_info['content'] = ''
+        p = content_ip_bottom.previous_sibling
+        while p:
+            if type(p) == bs4.element.NavigableString and p.strip():
+                content.append(re.sub(r'\s+', ' ', p.strip()))
+            p = p.previous_sibling
+        p = content_ip_bottom.next_sibling
+        # 以上原文是從下到上蒐集，所以必須倒過來才是原文順序
+        content.reverse()
+        # 以下原文是從上到下蒐集，所以append在後即可
+        while p:
+            if type(p) == bs4.element.NavigableString and p.strip():
+                content.append(re.sub(r'\s+', ' ', p.strip()))
+            p = p.next_sibling
+        post_info['content'] = ' '.join(content)
+
+        # 回覆
+        up, down, normal = 0, 0, 0
+        for element in soup.select('span.push-tag'):
+            if element.get_text(strip=True) == '推':
+                up += 1
+            elif element.get_text(strip=True) == '噓':
+                down += 1
+            else:
+                normal += 1
+        post_info['push_tags'] = {'up': up, 'down': down, 'normal': normal}
+
+        post_info['push_userids'] = [element.get_text(strip=True) for element in soup.select('span.push-userid')]
+        post_info['push_contents'] = [re.sub(r'^: ?', '', re.sub(r'\s+', ' ', element.get_text(strip=True))) for element in soup.select('span.push-content')]
+        post_info['push_ipdatetimes'] = [element.get_text(strip=True).strip() for element in soup.select('span.push-ipdatetime')]
+
+        post_info['url'] = str(response.url)
+        posts_info.append(post_info)
