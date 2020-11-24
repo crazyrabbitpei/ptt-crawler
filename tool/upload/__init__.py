b50efe32fe76a8882b649c421734be37fc9a5878
Your Name <you@example.com>
2020-11-24 16:40:39 +0800

    es連線資訊搬到env、偵測是否在aws機器上，是的話就使用aws auth連線

--- tool/upload/__init__.py
+++ tool/upload/__init__.py
@@ -1,23 +1,30 @@
-import asyncio
 from elasticsearch import Elasticsearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
 import boto3
 import json
 import os
-# For example, my-test-domain.us-east-1.es.amazonaws.com
-host = os.environ['HOST']
-region = os.environ['REGION']  # e.g. us-west-1
+import logging
 
+logger = logging.getLogger(__name__)
+
+host = os.getenv('ES_HOST')
+region = os.getenv('ES_REGION')  # e.g. us-west-1
 service = 'es'
+auth = None
+
 credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key,
-                   region, service, session_token=credentials.token)
+if credentials:
+    logger.info('By aws auth')
+    from requests_aws4auth import AWS4Auth
+    auth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+else:
+    logger.info('By basic auth')
+    auth = (os.getenv('ES_USER'), os.getenv("ES_PASSWD"))
 es = None
 def connect():
     global es
     es = Elasticsearch(
         hosts=[{'host': host, 'port': 443}],
-        http_auth=awsauth,
+        http_auth=auth,
         use_ssl=True,
         verify_certs=True,
         connection_class=RequestsHttpConnection)
